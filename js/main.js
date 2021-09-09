a1b9fce6c6af433026f9d2e2d3c0a5992f8d2a2d
KMininkov <74874576+Wordllban@users.noreply.github.com>
2021-09-09 12:59:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,5 @@ const OPEN_CLASSNAME = "open";
 const navLinks = document.getElementById("nav-links");
 
 function toggleMenu() {
-    if (navLinks.classList.contains(OPEN_CLASSNAME)) {
-        navLinks.classList.remove(OPEN_CLASSNAME);
-    } else {
-         navLinks.classList.add(OPEN_CLASSNAME);
-    }
+    navLinks.classList.toggle(OPEN_CLASSNAME);
 }
