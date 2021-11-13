c4049cf46f9d106a9dc09a9b7b7554d41ae5a926
KMininkov <mininkov3228@gmail.com>
2021-11-13 13:46:15 +0200

    added loading effect

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,11 +1,11 @@
+import { withRouter } from "react-router-dom";
 import { ChangeEvent, useEffect, useState } from "react";
 
 import { CatalogItem } from "./CatalogItem";
-
+import { Loader } from "./Loader";
+import { getFilteredData } from "../../services/api";
 
 import styles from "./Catalog.module.scss";
-import { withRouter } from "react-router-dom";
-import { getFilteredData } from "../../services/api";
 
 export interface dataProps {
     id: string | number;
@@ -19,16 +19,23 @@ const Catalog = () => {
     // search item
     const [titleFilter, setTitleFilter] = useState('')
     const [priceFilter, setPriceFilter] = useState('')
+    // loading effect
+    const [loading, setLoading] = useState(true)
     
     
     useEffect(() => {
         (async () => {
-            setItems( await getFilteredData(titleFilter, priceFilter));
+            setLoading(true);
+            setTimeout( async () => {
+                setItems( await getFilteredData(titleFilter, priceFilter));
+                setLoading(false);
+            }, 300)
         })()
     }, [priceFilter, titleFilter])
 
     // search by title
     const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setLoading(true);
         const enteredName: string = event.target.value;
         setTitleFilter(enteredName);
     }
@@ -37,9 +44,10 @@ const Catalog = () => {
     const [inputValue, setInputValue] = useState({value: ' '})
 
     const priceChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setLoading(true)  
         const enteredPrice: string | number = event?.currentTarget?.value 
         setInputValue({ value: event?.currentTarget?.value });
-        setPriceFilter(enteredPrice);        
+        setPriceFilter(enteredPrice);       
     }
 
     // show more
@@ -57,22 +65,27 @@ const Catalog = () => {
                 <div className={styles.filter}>
                     <div className={styles.filter__title}>
                         <input className={styles.input} type="text" name="search item" placeholder="Search device" onChange={inputHandler}/>
-                        <button className={styles.search_title}>Search</button>
                     </div>
+
+                    <p className={styles.slider__price}>Search item from price: {inputValue.value} $</p>
 
                     <div className={styles.filter__price}>
                         <input style={{ backgroundColor: "#202020", width: "400px"}} className={styles.slider} type="range" step="100" min="100" max="15000" value={inputValue.value} onChange={priceChange}/>
-                        <span className={styles.slider__price}>Search item from price: {inputValue.value} $</span>
-                        <button className={styles.search_price}>Search</button>
                     </div>
                 </div>
 
                 <div className={styles.wrapper}>                      
-                                {items?.length ? ( 
+                                { loading ? (
+                                    <Loader />
+                                ) : (
+                                items?.length ? ( 
                                     [...items.slice(0, count)].map( item => <CatalogItem key={item.id} id={item.id} title={item.model} price={item.price}/>)
                                 ) : (
                                     <h2>No Items Found</h2>
-                                )}
+                                    )
+                                )
+                            
+                            }
                 </div>
                     
                 <div className={styles.button__container}>
--- src/components/ItemPage/ItemPage.tsx
+++ src/components/ItemPage/ItemPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 // CRUD API
 import { getById } from "../../services/api";
+import { Loader } from "../Catalog/Loader";
 
 // styles
 import styles from "./ItemPage.module.scss"
@@ -11,24 +12,35 @@ import styles from "./ItemPage.module.scss"
 export const ItemPage = () => {
     const { id } = useParams<{id: string }>();    
     const [item, setItem] = useState<any>();
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         (async () => {
-            setItem(await getById(id));
+            setTimeout( async () => {
+                setLoading(false)
+                setItem(await getById(id));
+            }, 300)
         })()
     }, [id])
 
     return (
-        <div className={styles.item__page}>
-            <div className={styles.image__container}>
-                <img className={styles.image} src="https://images.unsplash.com/photo-1555543451-eeaff357e0f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80"  /* {item.image}  */
-                    alt="device"/>
-            </div>
-            <div className={styles.description}>
-                <h1 className={styles.title}>{item?.model}</h1>
-                <p className={styles.about}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa officia exercitationem ad ipsum dolorum ut corporis est eius soluta harum, magni omnis voluptatibus esse ducimus quasi sed temporibus et aperiam? Doloribus cumque nesciunt rem vero?</p>
-                <p className={styles.price}>${item?.price}</p>
-            </div>
+        <div >
+            { loading ? (
+                <Loader />
+            ) : (
+                <div className={styles.item__page}>
+                <div className={styles.image__container}>
+                    <img className={styles.image} src="https://images.unsplash.com/photo-1555543451-eeaff357e0f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80"  /* {item.image}  */
+                        alt="device"/>
+                </div>
+                <div className={styles.description}>
+                    <h1 className={styles.title}>{item?.model}</h1>
+                    <p className={styles.about}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa officia exercitationem ad ipsum dolorum ut corporis est eius soluta harum, magni omnis voluptatibus esse ducimus quasi sed temporibus et aperiam? Doloribus cumque nesciunt rem vero?</p>
+                    <p className={styles.price}>${item?.price}</p>
+                </div>
+                </div>
+            )
+            }
         </div>
     )
 }
