4e4d8504de4a2cbc06095f0c3c9fee2f67f02c9b
KMininkov <mininkov3228@gmail.com>
2021-12-07 18:58:50 +0200

    initial commit

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppSelector } from '../../hooks/redux.store.hooks'
 import { getCartProducts, getTotalPrice } from './cart.slice'
 import { CartItem } from './CartItem';
+import { OrderForm } from './OrderForm';
 
 import styles from "./Cart.module.scss";
 
 export const Cart: React.FC = () => {
     const totalPrice = useAppSelector(getTotalPrice);
     const cartProducts = useAppSelector(getCartProducts);
+    const [order, setOrder] = useState(false)
 
     return (
-        <div className={styles.cart}>
-            <h1 className={styles.title}>Cart</h1>
-            <div className={styles.items_container}>
-                {cartProducts.map(product => <CartItem key={product.id} id={product.id} model={product.model} price={product.price} amount={product.amount}/>)}
-            </div>
-            <div className={styles.order}>
-                <h5 className={styles.price}>Total price: {totalPrice} $</h5>
-                <button className={styles.button}>Make order</button>
-            </div>
-        </div>
+        <>
+            {order ? (
+                <OrderForm/>
+            ) : (
+                <div className={styles.cart}>
+                    <h1 className={styles.title}>Cart</h1>
+                    {cartProducts.length == 0 ? (
+                        <h3>Add something to cart!</h3>
+                    ) : (
+                        <>
+                            <div className={styles.items_container}>
+                                {cartProducts.map(product => <CartItem key={product.id} id={product.id} model={product.model} price={product.price} amount={product.amount}/>)}
+                            </div>
+                            <div className={styles.order}>
+                                <h5 className={styles.price}>Total price: {totalPrice} $</h5>
+                                <button className={styles.button} onClick={() => setOrder(!order)}>Make order</button>
+                            </div>
+                        </>
+                    )}
+                </div>
+            )}
+        </>
     );
 }
---
+++ src/components/Cart/OrderForm.tsx
@@ -0,0 +1,113 @@
+import styles from "./OrderForm.module.scss";
+
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const orderSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(30, 'Too Long!')
+        .required('Name field is Required'),
+    surname: Yup.string()
+        .min(2, 'Too Short!')
+        .max(30, 'Too Long!')
+        .required('Surname field is Required'),
+    email: Yup.string()
+        .email('Invalid email')
+        .required('Email is Required'),
+    country: Yup.string()
+        .min(2, 'Too Short!')
+        .required('Country is Required'),
+    city: Yup.string()
+        .min(2, 'Too Short!')
+        .required('City is Required'),
+    street: Yup.string()
+        .min(2, 'Too Sort')
+        .required('street is Required')
+});
+
+interface FormModel {
+    name: string,
+    surname: string,
+    email: string,
+    country: string,
+    city: string,
+    street: string,
+}
+
+export const OrderForm = () => {
+    return (
+        <div>
+            <Formik<FormModel> 
+            initialValues={{
+                name: "",
+                surname: "",
+                email: "",
+                country: "",
+                city: "",
+                street: "",
+            }}
+            validationSchema={orderSchema}
+            onSubmit={(values) => {
+                alert(JSON.stringify(values))
+            }}
+            >
+            {({handleSubmit, errors, touched}) => {
+                return (
+                    <Form className={styles.form} onSubmit={handleSubmit}>
+                        <div>
+                            <label className={styles.label} htmlFor="name">Name</label>
+                            <Field className={styles.field} name="name"/>
+                            {errors.name && touched.name ? (
+                                <div className={styles.error}>{errors.name}</div>
+                            ) : null}
+                        </div>
+
+                        <div>
+                            <label className={styles.label} htmlFor="surname">Surname</label>
+                            <Field className={styles.field} name="surname"/>
+                            {errors.surname && touched.surname ? (
+                                <div className={styles.error}>{errors.surname}</div>
+                            ) : null}
+                        </div>
+
+                        <div>
+                            <label className={styles.label} htmlFor="email">E-mail</label>
+                            <Field className={styles.field} name="email"/>
+                            {errors.email && touched.email ? (
+                                <div className={styles.error}>{errors.email}</div>
+                            ) : null}
+                        </div>
+                        
+                        <div>
+                            <label className={styles.label} htmlFor="country">Country</label>
+                            <Field className={styles.field} name="country"/>
+                            {errors.country && touched.country ? (
+                                <div className={styles.error}>{errors.country}</div>
+                            ) : null}
+                        </div>
+
+                        <div>
+                            <label className={styles.label} htmlFor="city">City</label>
+                            <Field className={styles.field} name="city"/>
+                            {errors.city && touched.city ? (
+                                <div className={styles.error}>{errors.city}</div>
+                            ) : null}
+                        </div>
+
+                        <div>
+                            <label className={styles.label} htmlFor="street">Street</label>
+                            <Field className={styles.field} name="street" />
+                            {errors.street && touched.street ? (
+                                <div className={styles.error}>{errors.street}</div>
+                            ) : null}
+                        </div>
+
+                        <button className={styles.order_button} type="submit">Make order</button>
+                    </Form>
+                )
+            }}
+            </Formik>
+        </div>
+    )
+}
