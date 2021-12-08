9fbce1affa5ba8fe1853aa93a0dffb66c76c2889
KMininkov <mininkov3228@gmail.com>
2021-12-09 01:13:25 +0200

    initial commit

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const Firebase = firebase.initializeApp({
-    apiKey: "AIzaSyCLxQ_yaroBBik1RfoLrPsp8ahJ41lIFag",
-    authDomain: "lab-12-e085a.firebaseapp.com",
-    projectId: "lab-12-e085a",
-    storageBucket: "lab-12-e085a.appspot.com",
-    messagingSenderId: "331278185114",
-    appId: "1:331278185114:web:00b595836bfb8d46246523" 
+    apiKey: "apikey",
+    authDomain: "authdomain",
+    projectId: "pid",
+    storageBucket: "storagebucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId" 
 });
 
 export const Providers = {
